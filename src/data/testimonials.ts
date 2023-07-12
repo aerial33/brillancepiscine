08558b5e6e31fd7d40299d2a8ea4d5526b3edb35
Fouad <fouadezari@gmail.com>
2023-07-12 19:07:11 +0200

    feat: add images to carousel

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -7,83 +7,83 @@ type Testimonials = {
 
 const testimonials: Testimonials[] = [
 	{
-		ville: "Merignac",
+		ville: "Yvrac",
 		temoignage:
-			"L'équipe de Douceur Piscine à Mérignac a été exceptionnelle du début à la fin de l'installation de notre piscine coque. Nous sommes très satisfaits du service et du produit final.",
+			"J'ai mandaté Mineral Piscine pour la réalisation d'une piscine coque, j'ai choisi un bassin rectangulaire de 4 mètres sur 8. Leur équipe a été très professionnelle, ponctuelle et le processus s'est déroulé sans accroc. Nous recommandons vivement leurs services à tous ceux qui envisagent d'installer une piscine.",
 		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/v1685545242/testimonials/piscine-merignac_ujaa7f.jpg",
-		size: 6,
-	},
-	{
-		ville: "Cambes",
-		temoignage:
-			"Notre piscine coque installée par Douceur Piscine à Cambe est devenue le point central de nos réunions familiales. L'équipe a été professionnelle et le résultat final est tout simplement incroyable.",
-		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/v1685545242/testimonials/piscine-cambe_vds14j.jpg",
-		size: 6,
-	},
-	{
-		ville: "Aygumorte les Graves",
-		temoignage:
-			"Nous recommandons vivement Douceur Piscine pour l'installation de piscines coques à Aygumorte les Graves. Leur équipe expérimentée a fait un excellent travail, et notre piscine est tout simplement magnifique !",
-		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1685545242/testimonials/piscine-aygumorte-les-graves_gqouua.jpg",
-		size: 7,
-	},
-	{
-		ville: "Naujac-sur-Mer",
-		temoignage:
-			"Nous sommes tellement heureux d'avoir choisi Douceur Piscine pour installer notre piscine coque à Naujac-sur-Mer. Leur équipe a été professionnelle, ponctuelle et le résultat final est tout simplement époustouflant. Nous recommandons vivement leurs services !",
-		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max/v1685545242/testimonials/piscine-naujac_cdcywr.jpg",
-		size: 7,
-	},
-	{
-		ville: "Camblanes-et-Meynac",
-		temoignage:
-			"Douceur Piscine a dépassé nos attentes avec l'installation de notre piscine coque à Camblanes-et-Meynac. L'équipe a été compétente et a réalisé un travail de grande qualité. Nous profitons maintenant de moments de détente et de divertissement dans notre propre havre aquatique.",
-		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/v1685545242/testimonials/piscine-camblane_byaq76.jpg",
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hairspray/v1689178860/testimonials/piscine-yvrac_bwxpez.jpg",
 		size: 8,
 	},
 	{
-		ville: "Parempuyre",
+		ville: "Saint Loubes",
 		temoignage:
-			"Nous sommes ravis d'avoir choisi Douceur Piscine pour l'installation de notre piscine coque à Parempuyre. Leur expertise et leur professionnalisme ont rendu le processus facile et le résultat est tout simplement splendide.",
+			"Nous sommes extrêmement satisfaits de notre piscine coque installée par Mineral Piscine. Leur équipe de pose a été très professionnelle et compétente. Nous sommes ravis du résultat final et recommandons leurs services à tous ceux qui souhaitent une piscine de qualité.",
 		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max/v1685546681/testimonials/piscine-parempuyre-2_pvmazc.jpg",
-		size: 7,
-	},
-	{
-		ville: "Pessac",
-		temoignage:
-			"L'installation de notre piscine coque à Pessac s'est déroulée sans accroc. Douceur Piscine a su répondre à toutes nos attentes, et maintenant nous avons notre propre oasis dans notre jardin. Merci !",
-		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max/v1685545243/testimonials/piscine-pessac_xpdxg4.jpg",
-		size: 5,
-	},
-	{
-		ville: "Talence",
-		temoignage:
-			"Talence a désormais une nouvelle référence en matière de piscines coques ! Douceur Piscine a réalisé un travail remarquable, avec une grande attention aux détails. Nous ne pourrions pas être plus satisfaits de notre choix.",
-		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max/v1685545242/testimonials/piscine-coque-talence_wesuao.jpg",
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_center,e_art:athena/v1689178859/testimonials/piscine-st-loubes_ehhq7w.jpg",
 		size: 6,
 	},
 	{
-		ville: "Vinsac",
+		ville: "Montussan",
 		temoignage:
-			"Nous sommes ravis de notre nouvelle piscine coque à Vinsac ! L'équipe d'installation de piscine a été professionnelle et efficace. Nous profitons désormais de moments de détente et de plaisir en famille.",
+			"Mineral Piscine a réalisé un travail exceptionnel lors de la pose de notre piscine coque. Leur équipe a été très attentive à nos besoins et a su créer un espace de détente parfait dans notre jardin.",
 		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1685545243/testimonials/piscine-Vinsac_ksgvvt.jpg",
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:athena/v1689178863/testimonials/piscine-montussan_lc5iq4.jpg",
+		size: 7,
+	},
+	{
+		ville: "Créon",
+		temoignage:
+			"Leur équipe a été très compétente et a su répondre à toutes nos exigences. Nous sommes ravis du résultat final et nous les recommandons vivement",
+		photo:
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_center,e_art:hokusai/v1689178859/testimonials/piscine-creon_jhac1c.jpg",
+		size: 7,
+	},
+	{
+		ville: "Izon",
+		temoignage:
+			"Mineral Piscine a été très professionnel et a su répondre à nos attentes pour l'installation de notre piscine coque à Izon. Malgré une problématique, l'équipe a su y répondre très rapidement. Nous profitons maintenant de moments de détente dans notre jardin aquatique.",
+		photo:
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1689178859/testimonials/piscine-izon_f4fov3.jpg",
+		size: 8,
+	},
+	{
+		ville: "Tresses",
+		temoignage:
+			"L'equipe de Mineral Piscine a été très professionnelle et a fait preuve d'un grand savoir-faire. Nous sommes impatients de profiter de notre piscine cet été et nous les recommandons sans hésitation.",
+		photo:
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1689178860/testimonials/piscine-tresses_zhqbvk.jpg",
+		size: 7,
+	},
+	{
+		ville: "Bouliac",
+		temoignage:
+			"Entreprise très professionnel du début jusqu'à la fin des travaux. Le commercial Mr V... s'est occupé de toutes les formalités administratives et a toujours été à l'écoute. Le gros oeuvre et l'installation de la piscine ont été réalisés dans les délais et en conformité avec notre demande. Je recommande pleinement cette société.",
+		photo:
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1689178860/testimonials/piscine-bouliac_kev4rj.jpg",
+		size: 5,
+	},
+	{
+		ville: "Fargues-Saint-Hilaire",
+		temoignage:
+			"Je suis très content d'avoir confié la réalisation de ma piscine à cette société que je recommande au plus grand nombre. Ma piscine est belle comme tout, le matériel additionnel comme la pompe à chaleur m'apporte un vrai confort en plus tout a été réalisé pour un prix compétitif.",
+		photo:
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill/v1689178859/testimonials/piscine-Fargues-Saint-Hilaire_d0uwgq.jpg",
+		size: 6,
+	},
+	{
+		ville: "Libourne",
+		temoignage:
+			"La pose de notre piscine coque a été une expérience sans stress grâce à l'équipe de professionnels. Ils ont pris en compte nos besoins et ont réalisé un travail impeccable. Nous sommes ravis du résultat final.",
+		photo:
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:athena/v1689178860/testimonials/piscine-libourne_etsm31.jpg",
 		size: 9,
 	},
 	{
 		ville: "Villenave d'Ornon",
 		temoignage:
-			"Villenave d'Ornon ne pourrait pas avoir de meilleur choix pour l'installation de piscines coques. Douceur Piscine a su réaliser notre vision avec précision et nous sommes enchantés par le résultat final.",
+			"J'ai rencontré les équipes de cette société sur la foire de Bordeaux et nous avons fait affaire pour la réalisation d'une piscine coque. L'étude du projet a été parfaitement menée par le commercial. Et les techniciens ont eux aussi œuvrés dans les règles de l'art.",
 		photo:
-			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1685545243/testimonials/piscine-villenave-ornon_cfl08g.jpg",
+			"https://res.cloudinary.com/dp6synsa6/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:athena/v1689178860/testimonials/pisciniste_villenave-ornon_lvz2a9.jpg",
 		size: 4,
 	},
 ]
